9f98b6a87ca5b2c0d6929edac3fc492e2d7c3387
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-04-16 15:05:54 -0300

    implementação de fila e pilha

---
+++ pilha01.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+class noh {
+    friend class pilha;
+    private:
+        int mValor;
+        noh* mProximo;
+    public:
+        noh(int valor);
+};
+
+noh:: noh(int valor) {
+    mValor = valor;
+    mProximo = NULL;
+}
+
+class pilha {
+    private:
+        noh* mTopo;
+        unsigned mTamanho;
+
+    public: 
+        pilha();
+        ~pilha();
+        unsigned tamanho();
+        void empilha(int valor);
+        int desempilha();
+        void limpaPilha();
+        bool vazia();
+};
+
+pilha::pilha() {
+    mTopo = NULL;
+    mTamanho = 0;
+}
+
+pilha::~pilha() {
+    limpaPilha();
+}
+
+void pilha::limpaPilha() {
+    while(mTamanho > 0) {
+        desempilha();
+    };
+}
+
+unsigned pilha::tamanho() {
+    return mTamanho;
+}
+
+bool pilha::vazia() {
+    return (mTamanho == 0);
+}
+
+void pilha::empilha(int valor){
+    cout << "topo (antes): " << mTopo << endl;
+    noh* novo = new noh(valor);
+    novo->mProximo = mTopo;
+    mTopo = novo;
+    mTamanho++;
+    cout << "topo (depois): " << mTopo << endl;
+    cout << "topo (próximo do topo): " << mTopo->mProximo << endl;
+}
+
+int pilha::desempilha() {
+    int valor = mTopo->mValor;
+    noh* temp = mTopo;
+    mTopo = mTopo->mProximo;
+    delete temp;
+    mTamanho--;
+    return valor;
+}
+
+int main () {
+    pilha p1;
+    int num;
+    cin >> num;
+
+    while (num >= 0) {
+        p1.empilha(num);
+        cin >> num;
+    }
+
+    cout << "tamanho:" << p1.tamanho() << endl;
+
+    cout << "elementos: ";
+
+    while (not p1.vazia()) {
+        cout << p1.desempilha() << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
